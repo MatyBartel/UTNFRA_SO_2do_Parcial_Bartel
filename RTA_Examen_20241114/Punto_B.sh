9189eb2650524b015b075dae25815401d7551600
MatyBartel <matiasbartel128@gmail.com>
2024-11-14 20:53:33 +0000

    Punto A y B - Resolucion LVM y Bash Scripting

---
+++ RTA_Examen_20241114/Punto_B.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cat << 'FIN' > /tmp/BartelAltaUser-Groups.sh
+#!/bin/bash
+
+USUARIO_REF=$1
+LISTA=$2
+
+CONTRA=$(sudo grep "$USUARIO_REF" /etc/shadow | awk -F ':' '{print $2}')
+
+ANT_IFS=$IFS
+IFS=$'\n'
+
+for i in $(cat "$LISTA" | awk -F ',' '{print $1 " " $2 " " $3}' | grep -v '^#'); do
+    USUARIO=$(echo "$i" | awk '{print $1}')
+    GRUPO=$(echo "$i" | awk '{print $2}')
+    HOME_USR=$(echo "$i" | awk '{print $3}')
+
+    sudo groupadd -f "$GRUPO"
+    sudo useradd -m -d "$HOME_USR" -g "$GRUPO" "$USUARIO"
+    echo "$USUARIO:$CONTRA" | sudo chpasswd -e
+done
+
+IFS=$ANT_IFS
+FIN
+
+sudo mv /tmp/BartelAltaUser-Groups.sh /usr/local/bin/BartelAltaUser-Groups.sh
+sudo chmod 755 /usr/local/bin/BartelAltaUser-Groups.sh
+
+/usr/local/bin/BartelAltaUser-Groups.sh bartel /home/bartel/UTN-FRA_SO_Examenes/202406/bash_script/Lista_Usuarios.txt
